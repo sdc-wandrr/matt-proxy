9cd6580174a71a505309ac5ce081fe4e1c8cf8ea
Evan Kolb <kongaiwen@gmail.com>
2020-08-17 15:27:19 -0700

    Use http-proxy-middleware to handle api targets

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware }= require('http-proxy-middleware');
 const app = express();
 const PORT = 3030;
 
@@ -7,19 +8,21 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-
 app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
 
-// app.get('/api/hostels/:hostel_id/images', (req, res) => {
+// API reroutes
 
-// });
-// app.get('/house/:id', (req, res) => {
-//   res.redirect('http://localhost:3000/house/:id')
-// });
+// Image carousel service
+app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http://localhost:3007', changeOrigin: true }));
+// availability service
+app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({ target: 'http://localhost:3009', changeOrigin: true }));
+// description map rules service
+app.use('/house/:id/hostel', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+app.use('/house/:id/description', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+app.use('/house/:id/rules', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+app.use('/house/:id/address', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
+// reviews service
+app.use('/api/reviews', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
-// app.get('/public/bundle.js');
-// app.get();
-// app.get();
-// app.get();
 
 app.listen(PORT, () => console.log('Proxy Server listening on port ', PORT));
