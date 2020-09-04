e98051076e1e5c368a882cd0ed6ecd8619432a38
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-09-04 14:48:24 -0700

    Adds New Relic information for testing - updates routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware }= require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const PORT = 3030;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
