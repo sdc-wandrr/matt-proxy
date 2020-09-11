2ad4f234cf07bd1dd9743ababe5085fa68340672
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-09-11 15:53:13 -0700

    Adds correct end points for API`:`:

--- server/index.js
+++ server/index.js
@@ -22,10 +22,10 @@ app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http:
 // availability service
 // app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
 // description map rules service
-app.use('api/house/:id/hostel', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
-app.use('api/house/:id/description', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
-app.use('api/house/:id/rules', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
-app.use('api/house/:id/address', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
+app.use('/api/house/:id/hostel', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
+app.use('/api/house/:id/description', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
+app.use('/api/house/:id/rules', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
+app.use('/api/house/:id/address', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
 // reviews service
 app.use('/hostels/:id/api/reviews', createProxyMiddleware({ target: 'http://100.26.204.252:3001/', changeOrigin: true }));
 
