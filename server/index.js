de1a1da63df39a7c9def1b1e93ae18efe50914cb
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-09-11 20:26:59 -0700

    Removes unused enpoints and cleans up spec testing

--- server/index.js
+++ server/index.js
@@ -11,23 +11,17 @@ app.use((req, res, next) => {
 });
 app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
 
+// loaderio verification
 app.get('/loaderio-12556c443590d1d7ab0e96b850dcc72a/', (req, res) => {
-  res.sendFile(path.join(__dirname, './Test/loaderio-12556c443590d1d7ab0e96b850dcc72a.txt'));
+  res.sendFile(path.join(__dirname, './spec/loaderio-12556c443590d1d7ab0e96b850dcc72a.txt'));
 });
 
 // API reroutes
-
-// Image carousel service
 app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http://172.31.30.142', changeOrigin: true }));
-// availability service
-// app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
-// description map rules service
 app.use('/api/house/:id/hostel', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
 app.use('/api/house/:id/description', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
 app.use('/api/house/:id/rules', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
 app.use('/api/house/:id/address', createProxyMiddleware({ target: 'http://54.215.89.194', changeOrigin: true }));
-// reviews service
 app.use('/hostels/:id/api/reviews', createProxyMiddleware({ target: 'http://100.26.204.252:3001/', changeOrigin: true }));
-
 
 app.listen(PORT, () => console.log('Proxy Server listening on port ', PORT));
